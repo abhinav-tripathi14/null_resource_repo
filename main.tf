7e267455770cbf0e3a9a59de22abe3468fafa3c3
Abhinav Tripathi <abhi.tripathi14@gmail.com>
2024-04-21 21:17:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.18.0"
+    }
+  }
+}
+
 resource "null_resource" "null_1" {
 count =2
 }
